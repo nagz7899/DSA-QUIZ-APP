2582e8589955cc79a06bc2a93f02960a16ad109e
SAMPATH NAG <61879294+nagz7899@users.noreply.github.com>
2021-07-25 11:57:07 +0530

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,117 @@
+
+let questions = [
+    {
+    numb: 1,
+    question: "Process of inserting an element in stack is called",
+    answer: "Push",
+    options: [
+      "Create",
+      "Push",
+      "Pop",
+      "Evaluate"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Process of removing an element from stack is called",
+    answer: "Pop",
+    options: [
+      "Pop",
+      "Push",
+      "Queue",
+      "Stack"
+    ]
+  },
+    {
+    numb: 3,
+    question: "In a stack, if a user tries to remove an element from an empty stack it is called",
+    answer: "Underflow",
+    options: [
+      "Underflow",
+      "Empty collection",
+      "Zero collection",
+      "Garbage Collection"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Pushing an element into stack already having five elements and stack size of 5, then stack becomes",
+    answer: "Overflow",
+    options: [
+      "Overflow",
+      "Crash",
+      "Underflow",
+      "None"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Consider the usual algorithm for determining whether a sequence of parentheses is balanced. The maximum number of parentheses that appear on the stack AT ANY ONE TIME when the algorithm analyzes: (()(())(()))?",
+    answer: "3",
+    options: [
+      "1",
+      "2",
+      "3",
+      "4 or More"
+    
+    ]
+  },
+  {
+    numb: 6,
+    question: "Consider the usual algorithm for determining whether a sequence of parentheses is balanced. Suppose that you run the algorithm on a sequence that contains 2 left parentheses and 3 right parentheses (in some order). The maximum number of parentheses that appear on the stack AT ANY ONE TIME during the computation?",
+    answer: "2",
+    options: [
+      "1",
+      "2",
+      "3",
+      "4"
+    ]
+  },
+  {
+    numb: 7,
+    question: "What is the value of the postfix expression 6 3 2 4 + – *?",
+    answer: "-18",
+    options: [
+      "1",
+      "20",
+      "40",
+      "-18"
+    ]
+  },
+  {
+    numb: 8,
+    question: "A linear list of elements in which deletion can be done from one end (front) and insertion can take place only at the other end (rear) is known as",
+    answer: "Queue",
+    options: [
+      "Queue",
+      "Stack",
+      "Tree",
+      "List"
+    ]
+  },
+  {
+    numb: 9,
+    question: "A queue follows",
+    answer: "FIFO",
+    options: [
+      "FIFO",
+      "LIFO",
+      "ORDERED",
+      "LINEAR"
+    ]
+  },
+  {
+    numb: 10,
+    question: "Circular Queue is also known as",
+    answer: "Ring Buffer",
+    options: [
+      "Ring Buffer",
+      "Square Buffer",
+      "Circular Buffer",
+      "Linear Buffer"
+    ]
+  },
+ 
+  
+
+];
